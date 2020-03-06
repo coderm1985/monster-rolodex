dd9482c7e43b468193e07ca632f4daaf3532f0c4
coderm1985 <coderm1985@gmail.com>
2020-03-06 13:23:10 +0530

    Updates

---
+++ precache-manifest.f1fed107414843542a39a6d4468cbb9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20841de1168b09f942b33c422139818a",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bd0731d14a60e028a166",
+    "url": "/monster-rolodex/static/css/main.88a5f64d.chunk.css"
+  },
+  {
+    "revision": "4a9667cafe01c67a5b34",
+    "url": "/monster-rolodex/static/js/2.5f911116.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.5f911116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd0731d14a60e028a166",
+    "url": "/monster-rolodex/static/js/main.ad978fb0.chunk.js"
+  },
+  {
+    "revision": "4d8b78c1e7ae0adfd38a",
+    "url": "/monster-rolodex/static/js/runtime-main.7e534d31.js"
+  }
+]);
