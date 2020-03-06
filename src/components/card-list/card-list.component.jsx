584fcd51a70857af355c39a5ec811ca37d45438c
coderm1985 <coderm1985@gmail.com>
2020-03-06 12:48:10 +0530

    pushing changes for rolodex

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Card} from '../card/card.component';
+import './card-list.styles.css';
+
+
+// Functional component accepts props as default parameter
+export const CardList = (props) => {
+    return (
+        <div className='card-list'>
+            {
+                props.monsters.map(monster => (
+                    <Card key={monster.id} monster={monster} />
+                ))
+            }
+        </div>
+    )
+};
